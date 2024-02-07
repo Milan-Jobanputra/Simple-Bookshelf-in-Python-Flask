481368b30c2e484f84b3a5387977a62cf9b93d9e
APKAWLGRM6OXZSDK7HMI <wpexpert8@gmail.com>
2024-02-07 12:49:23 +0530

    Error app log module

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, render_template, request, redirect, url_for
 
 '''
@@ -15,17 +16,32 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.ERROR)
+
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+     return render_template("index.html")
 
 
 @app.route("/add")
 def add():
-    pass
+    try:
+        # Code that may raise an error
+      
+         # / 0  # Division by zero to generate an error
+        pass
+
+    except Exception as e:
+        # Log the error
+        # Log the error with details
+        logging.error(f"An error occurred: {str(e)}", exc_info=True)
+        # Return error message to the user
+        return jsonify({'error': f"An error occurred: {str(e)}"}), 500
+    
 
 
 if __name__ == "__main__":
