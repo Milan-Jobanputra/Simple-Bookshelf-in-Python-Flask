2a50f31e5b631e75a295087d1ce55a6c477ad1fe
APKAWLGRM6OXZSDK7HMI <wpexpert8@gmail.com>
2024-02-08 16:26:08 +0530

    add and store entry in book dict

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 from flask import Flask, render_template, request, redirect, url_for
+from flask import jsonify
+
 
 '''
 Red underlines? Install the required packages first: 
@@ -24,24 +26,34 @@ all_books = []
 
 @app.route('/')
 def home():
-     return render_template("index.html")
+     return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    try:
-        # Code that may raise an error
+    try: 
+       if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+        
+         
+        # e.g. in this case to the home page after the form has been submitted.
+        return redirect(url_for('home'))
       
-         # / 0  # Division by zero to generate an error
-        pass
+        
 
     except Exception as e:
         # Log the error
         # Log the error with details
         logging.error(f"An error occurred: {str(e)}", exc_info=True)
         # Return error message to the user
-        return jsonify({'error': f"An error occurred: {str(e)}"}), 500
-    
+        #return jsonify({'error': f"An error occurred: {str(e)}"}), 500
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
