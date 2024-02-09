54b926db5ad7610c744ff541b38ea10be75988e8
APKAWLGRM6OXZSDK7HMI <wpexpert8@gmail.com>
2024-02-09 11:41:47 +0530

    sqlmodel import

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 from flask import Flask, render_template, request, redirect, url_for
 from flask import jsonify
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 
 app = Flask(__name__)
